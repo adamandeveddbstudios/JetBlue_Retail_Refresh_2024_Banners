847bd00597c35666714faae090353acc1f96e793
Mahesh Bhatt <141926871+darkCoderMahesh@users.noreply.github.com>
2024-07-23 18:41:43 +0530

    IFE non dynamic

---
+++ JetBlue_Retail_Refresh_2024_Banners/IFE/non-dynamic/richLoads/JB_Retail_24_IFE_NonDynamic_728x90_RL1/mainRL1.js
@@ -0,0 +1,91 @@
+// Global transition speed
+var transitionSpeed = 0.5;
+
+// Global ease setting
+var easing = Power4.easeOut;
+
+// Banner duration timer start time
+var startTime;
+
+// Timeline reference
+var tl, tl2;
+
+////////////////////////////////////////////////////////////////////////
+// @FT1 - code block start
+//VARIABLE DECLARATIONS
+var ctURL = "";
+
+
+var default_exit = myFT.$("#default_exit");
+
+var default_exit = myFT.$("#default_exit");
+var clickTag1_url="";
+
+//
+default_exit.on('click',function(){
+  myFT.clickTag(1,clickTag1_url)
+})
+// wait for instantads to load before initializing creative animation
+myFT.on('instantads',function(){
+
+  clickTag1_url=myFT.instantAds.clickTag1_url;
+  myFT.dispatch('RL1_available');
+
+})
+myFT.on('theFeedLoaded', function(e) {
+  myFT.dispatch('RL1_ready_to_play');
+});
+myFT.on('RL1_play', function () {
+  init();
+});
+// @FT1 - code block end
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+// Init tricggered by onLoad in Body tag
+function init() {
+  // Set Banner duration timer
+  startTime = new Date();
+
+  // Set Global Timeline
+  tl = new TimelineMax({onComplete:endTime});
+
+  animate();
+  setRollover();
+
+}
+
+function animate() {
+  //make parent (base file) border black
+
+  tl.set(["#main_content"], { autoAlpha: 1, force3D: true });
+
+  tl.addLabel('frame1', 0)
+  .to('#h1', 0.5, { autoAlpha: 1, ease: Power1.easeOut}, 'frame1')
+
+  .addLabel('frame2', 'frame1+=3')
+  .to(['#h2'], 0.5, { autoAlpha: 1, ease: Power1.easeOut }, "frame2+=0.5")
+
+}
+
+// CTA grow on hover
+
+function setRollover() {
+  document.getElementById('default_exit').addEventListener('mouseover', defaultOver, false);
+  document.getElementById('default_exit').addEventListener('mouseout', defaultOut, false);
+}
+
+function defaultOver() {
+  TweenMax.to('#cta', 0.25, { scale: 1.05, ease: Power1.easeInOut })
+}
+
+function defaultOut() {
+  TweenMax.to('#cta', 0.25, { scale: 1, ease: Power1.easeInOut })
+}
+
+// End timer
+function endTime(){
+  // show total banner animation time in browser console.
+  var endTime = new Date()
+  console.log("Animation duration: " + ((endTime - startTime) / 1000) + " seconds");
+}
