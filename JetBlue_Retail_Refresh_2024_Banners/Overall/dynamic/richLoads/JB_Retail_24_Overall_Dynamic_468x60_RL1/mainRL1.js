0762b383d784875b53b63bb55a72cf9a74dfa4e1
Mahesh Bhatt <141926871+darkCoderMahesh@users.noreply.github.com>
2024-07-30 21:44:04 +0530

    Overall dynamic

--- JetBlue_Retail_Refresh_2024_Banners/Overall/dynamic/richLoads/JB_Retail_24_Overall_Dynamic_468x60_RL1/mainRL1.js
+++ JetBlue_Retail_Refresh_2024_Banners/Overall/dynamic/richLoads/JB_Retail_24_Overall_Dynamic_468x60_RL1/mainRL1.js
@@ -110,11 +110,10 @@ function animate() {
   .to('#h1', 0.5, { autoAlpha: 1, ease: Power1.easeOut}, 'frame1')
 
   .addLabel('frame2', 'frame1+=3')
-  .to(['#h2', '#terms1'], 0.5, { autoAlpha: 1, ease: Power1.easeOut }, "frame2+=1")
+  .to(['#h2'], 0.5, { autoAlpha: 1, ease: Power1.easeOut }, "frame2+=1")
 
   .addLabel('frame_END', "frame2+=4")
   .to('#endframeBg', 0.6 ,{ left: 0, ease: Back.easeOut.config(.3)}, 'frame_END')
-  .to('#terms1', 0.5, { autoAlpha: 0, ease: Power1.easeOut}, 'frame_END')
 
 
   ////////////////////////////////////////
