4c447afdd363f11e07915a668b8c323459ccfa9f
Mahesh Bhatt <141926871+darkCoderMahesh@users.noreply.github.com>
2024-08-09 16:30:11 +0530

    Wifi non dynamic

--- JetBlue_Retail_Refresh_2024_Banners/WiFi/non-dynamic/richLoads/JB_Retail_24_WiFi_NonDynamic_300x250_RL1/mainRL1.js
+++ JetBlue_Retail_Refresh_2024_Banners/WiFi/non-dynamic/richLoads/JB_Retail_24_WiFi_NonDynamic_300x250_RL1/mainRL1.js
@@ -1,31 +1,11 @@
-// Global transition speed
-var transitionSpeed = 0.5;
-
-// Global ease setting
-var easing = Power4.easeOut;
-
-// Banner duration timer start time
-var startTime;
-
-// Timeline reference
 var tl;
-
-
-// myFT.on('theFeedLoaded', function(e) {
-// console.log('RL1: Richload recieved feed from Base file)');
-// feedLoaded(e.a);
-// });
-// myFT.on('RL1_play', function () {
-// init();
-// });
-
-////////////////////////////////////////////////////////////////////////
-// @FT1 - code block start
-//VARIABLE DECLARATIONS
+var tl2;
+var container = myFT.$("#main_container");
 var default_exit = myFT.$("#default_exit");
 
-var default_exit = myFT.$("#default_exit");
+// var myVid=myFT.$("#video1");
 var clickTag1_url="";
+// var vid_time_triggered=false;
 
 //
 default_exit.on('click',function(){
@@ -35,32 +15,11 @@ default_exit.on('click',function(){
 myFT.on('instantads',function(){
 
   clickTag1_url=myFT.instantAds.clickTag1_url;
-  myFT.dispatch('RL1_available');
 
 })
-myFT.on('theFeedLoaded', function(e) {
-  myFT.dispatch('RL1_ready_to_play');
-});
-myFT.on('RL1_play', function () {
-  init();
-});
-// @FT1 - code block end
-///////////////////////////////////////////////////////////////////////////////////////
 
 
-
-// Init tricggered by onLoad in Body tag
-function init() {
-  // Set Banner duration timer
-  startTime = new Date();
-
-  // Set Global Timeline
-  tl = new TimelineMax({onComplete:endTime});
-
-  animate();
-  setRollover();
-
-}
+init()
 
 function animate() {
   tl.set(["#main_content"], { autoAlpha: 1, force3D: true });
@@ -71,10 +30,6 @@ function animate() {
   .addLabel('frame2', 'frame1+=3')
   .to(['#h2', '#terms1'], 0.5, { autoAlpha: 1, ease: Power1.easeOut }, "frame2+=0.5")
 
-  // .addLabel('frame_END', "frame2+=4")
-  // .to('#endframeBg', 0.6 ,{ top: 0, ease: Back.easeOut.config(.3)}, 'frame_END')
-  // .to('#terms1', 0.5, { autoAlpha: 0, ease: Power1.easeOut}, 'frame_END')
-
 }
 
 function setRollover() {
@@ -83,21 +38,23 @@ function setRollover() {
 }
 
 function defaultOver() {
-  TweenMax.to('#cta', 0.25, { scale: 1.05, ease: Power1.easeInOut })
+  TweenMax.to('#cta', 0.15, { scale: 1.1, ease: Power1.easeInOut })
 }
 
 function defaultOut() {
-  TweenMax.to('#cta', 0.25, { scale: 1, ease: Power1.easeInOut })
-}
-
-// End timer
-function endTime(){
-  // show total banner animation time in browser console.
-  var endTime = new Date()
-  console.log("Animation duration: " + ((endTime - startTime) / 1000) + " seconds");
+  TweenMax.to('#cta', 0.15, { scale: 1, ease: Power1.easeInOut })
 }
 
 
+
+function init() {
+
+  tl = new TimelineMax();
+
+  animate();
+  setRollover();
+		
+}
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("year").innerHTML = year;
