8c99d2f71a1317f9dc7dfd9f01272f0dab77987a
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2024-08-23 15:54:20 +0530

    Add files via upload

--- JetBlue_Retail_Refresh_2024_Banners/WiFi/dynamic/richLoads/JB_Retail_24_Wifi_Dynamic_468x60_RL2/mainRL2.js
+++ JetBlue_Retail_Refresh_2024_Banners/WiFi/dynamic/richLoads/JB_Retail_24_Wifi_Dynamic_468x60_RL2/mainRL2.js
@@ -74,8 +74,8 @@ function setupContent() {
     startingAt[0].style.display = 'none';
 
   } else {
-    // h4[0].innerHTML = "<span class='sm blue'>Fly from </span><br><span class='blue city'>" + "New York City" + "</span> <span class='blue to'>to</span> <span class='blue city'>" + "Turks & Caicos" + "</span>"; // - Populates main headline
-    h4[0].innerHTML = "Fly from <br><span class='city cityLineBreak gap'>" + origin_longform + "</span><br><span class='to'>to</span> <span class='city cityLineBreak'>" + destination_longform + "</span>";
+    h4[0].innerHTML = "Fly from <br><span class='city'>" + "Boston" + "</span><br><span class='to'>to</span> <span class='city'>" + "West Palm Beach" + "</span>";
+    // h4[0].innerHTML = "Fly from <br><span class='city'>" + origin_longform + "</span><br><span class='to'>to</span> <span class='city'>" + destination_longform + "</span>";
 
     const myH4 = document.getElementById('h4')
     const myCity = document.getElementsByClassName('city')
